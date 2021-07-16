dc8f39f018cba949ac547bb965a269e4c5f9e808
sethu <ssethucse@gmail.com>
2021-07-16 15:19:23 +0530

    Initial commit

---
+++ src/main/java/com/pos/config/apigateway/config/LoggingFilter.java
@@ -0,0 +1,20 @@
+package com.pos.config.apigateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+    private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Logger Info = {}",exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
---
+++ src/test/java/com/pos/config/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.config.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
